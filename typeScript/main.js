84e0a3d1e4a520cd3941a7c6775551bc8a0427da
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-09 17:06:44 +0900

    ✨ 배열 타입 정하기

--- typeScript/main.js
+++ typeScript/main.js
@@ -1,4 +1,5 @@
 "use strict";
+// 올바른 타입 지정하기
 let monsterName;
 let level;
 let hasGold;
@@ -13,3 +14,13 @@ console.log(`${monsterName}의 레벨은 ${level}이고,\n` +
     `${hasGold ? '해치우면 골드를 얻는' : '해치워도 골드를 주지 않는'} 몬스터입니다.\n` +
     `${skill ? `가진 능력은 ${skill} 이고 \n` : ''}` +
     `${area ? `출현 지역은 ${area} 입니다.` : ''}`);
+// 타입 배열 정하기
+const current = [0, 0];
+const target = [4, 5];
+const dx = target[0] - current[0];
+const dy = target[1] - current[1];
+console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+const items = [];
+items.push('갑옷');
+items.push('빨간 물약');
+console.log(`${items.join(', ')}을/를 획득했다!`);
--- typeScript/main.ts
+++ typeScript/main.ts
@@ -1,3 +1,4 @@
+// 올바른 타입 지정하기
 let monsterName: string;
 let level : number;
 let hasGold : boolean;
@@ -16,3 +17,15 @@ console.log(
     `${skill ? `가진 능력은 ${skill} 이고 \n` : ''}` +
     `${area ? `출현 지역은 ${area} 입니다.` : ''}`
 );
+
+// 타입 배열 정하기
+const current: [number, number] = [0, 0];
+const target: [number, number] = [4, 5];
+const dx = target[0] - current[0];
+const dy = target[1] - current[1];
+console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+
+const items: string[]= [];
+items.push('갑옷');
+items.push('빨간 물약');
+console.log(`${items.join(', ')}을/를 획득했다!`);
