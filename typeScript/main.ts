2ca7830daed1f8c707984953f0b708709b14f66d
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-09 16:57:13 +0900

    ✨ 올바른 type 지정하기

--- typeScript/main.ts
+++ typeScript/main.ts
@@ -1,5 +1,18 @@
-let itemName: string = '코드잇 블랙 후드';
-let itemPrice: number = 129000;
-let membersOnly: boolean = true;
-let owner: undefined = undefined;
-let seller: null = null;
+let monsterName: string;
+let level : number;
+let hasGold : boolean;
+let skill : undefined;
+let area : null;
+
+monsterName = '코드잇 고블린';
+level = 255;
+hasGold = false;
+skill = undefined;
+area = null;
+
+console.log(
+  `${monsterName}의 레벨은 ${level}이고,\n` +
+    `${hasGold ? '해치우면 골드를 얻는' : '해치워도 골드를 주지 않는'} 몬스터입니다.\n` +
+    `${skill ? `가진 능력은 ${skill} 이고 \n` : ''}` +
+    `${area ? `출현 지역은 ${area} 입니다.` : ''}`
+);
