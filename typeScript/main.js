e84ab6f49fc1a2aa6830af8a190337d15a381d4d
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-09 17:21:10 +0900

    ✨ 표에 맞게 type 추가하기

--- typeScript/main.js
+++ typeScript/main.js
@@ -24,3 +24,14 @@ const items = [];
 items.push('갑옷');
 items.push('빨간 물약');
 console.log(`${items.join(', ')}을/를 획득했다!`);
+// 표를 참고해 타입 지정하기
+const monster = {
+    name: '고블린',
+    level: 22,
+    skills: [
+        '태권도', '특공무술'
+    ]
+};
+console.log(`${monster.name}의 레벨은 ${monster.level}이고,\n` +
+    `${monster.hasGold ? '해치우면 골드를 얻는' : '해치워도 골드를 주지 않는'} 몬스터입니다.\n` +
+    `${monster.skills.length > 0 ? `가진 능력은 ${monster.skills.join(', ')}입니다.` : ''}`);
